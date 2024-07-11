9d0fd90eb4d00ea12a6343ca59021f1a2969cc04
Tom Morris Feist <morris.feist@googlemail.com>
2024-07-11 09:44:37 +0200

    Remove typst from flake inputs

--- flake.nix
+++ flake.nix
@@ -1,32 +1,21 @@
 {
   description = "CV PDF generator";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    typst.url = "github:typst/typst";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, typst, self, ... }@inputs:
+  outputs = { nixpkgs, ... }@inputs:
     let
       systems = builtins.attrNames nixpkgs.legacyPackages;
 
-      forAllSystems = function: nixpkgs.lib.genAttrs systems
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = builtins.attrValues self.overlays;
-              config.allowUnfree = true;
-            };
-          in
-          function pkgs
-        );
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in f pkgs
+      );
     in
     {
       packages = forAllSystems (pkgs: import ./packages.nix { inherit pkgs inputs; });
       devShells = forAllSystems (pkgs: {
         default = pkgs.callPackage ./shell.nix { inherit inputs; };
       });
-      overlays = import ./overlays.nix { inherit inputs; };
     };
 }
